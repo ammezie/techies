49a8e167192f295193acf76131adf85e87f5f94a
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-04-10 16:35:50 +0100

    setup Vue Apolo

--- src/main.js
+++ src/main.js
@@ -1,15 +1,49 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/' })
+
+const httpLinkAuth = setContext((_, { headers }) => {
+  // get the authentication token from localstorage if it exists
+  const token = localStorage.getItem('USER_TOKEN')
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLinkAuth.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+// Install the vue plugin
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  provide: apolloProvider.provide(),
   components: { App },
   template: '<App/>'
 })
